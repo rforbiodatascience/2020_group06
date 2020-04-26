77e23c20ece7535e7c49e6a4a7ef81b1e6b31888
saafdk <saafdk@gmail.com>
2020-04-26 18:54:29 +0200

    # stagged

--- R/99_func.R
+++ R/99_func.R
@@ -1,2 +1,31 @@
 # Define project functions
 # ------------------------------------------------------------------------------
+
+# patient_data_first_df %>% select(province) %>% anti_join(y = daily_covid_trends_df, by = c('province' = 'province')) %>% group_by(province) %>% count() %>% print(n = Inf)
+# ts_confirmed_world_df %>% select(country) %>% anti_join(y = daily_covid_trends_df, by = c('country' = 'country')) %>% group_by(country) %>% count() %>% print(n = Inf)
+#
+# patient_data_first_df %>% select(province) %>% group_by(province) %>% count() %>% print(n = Inf)
+# daily_covid_trends_df %>% select(province) %>% group_by(province) %>% count() %>% print(n = Inf)
+#
+# patient_data_first_df %>% select(country) %>% group_by(country) %>% count() %>% print(n = Inf)
+# daily_covid_trends_df %>% select(country) %>% group_by(country) %>% count() %>% print(n = Inf)
+
+# patient_data_first_df %>%
+#   select(symptoms_set) %>%
+#   separate_rows(symptoms_set, sep = ', ') %>%
+#   group_by(symptoms_set) %>%
+#   count() %>%
+#   print(n = Inf)
+
+# na_if(df3, 'N/A')
+
+# patient_data_first_df %>%
+#   filter_all(any_vars(str_detect(., 'N/A')))
+
+
+# patient_data_second_df %>% select(country, province) %>% anti_join(y = daily_covid_trends_df) %>% group_by(country, province) %>% count() %>% print(n = Inf)
+#
+# ts_confirmed_world_df %>% select(province) %>% group_by(province) %>% count() %>% print(n = Inf)
+# patient_data_second_df %>% select(province) %>% anti_join(y = patient_data_first_df, by = c('province' = 'province')) %>% group_by(province) %>% count() %>% print(n = Inf)
+#
+# ts_confirmed_world_df %>% select(country) %>% group_by(country) %>% count() %>% print(n = Inf)
