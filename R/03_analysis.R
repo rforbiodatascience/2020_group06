e5acfaa076dcfaf74ebab8afbe8dec2f25d5c729
saafdk <saafdk@gmail.com>
2020-04-30 14:32:42 +0200

    Containing models for patient_data; pca, decision-tree (predicting is_dead), logistic regression (using tidy and augment). The modelling is not performed with test and train.

--- R/03_analysis.R
+++ R/03_analysis.R
@@ -11,7 +11,8 @@ library("leaflet.extras")
 library("ggpubr")
 library("broom")
 library("purrr")
-
+library("rpart")
+library("rpart.plot")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -156,17 +157,27 @@ ggarrange(p1, p2, p3, p4, ncol=2, nrow=2, align = "h")
 
 
 
-####################################################################################
-########## Comparison of Denmark, Sweden, Romania, Turkey and Philippines ##########
+###############################################################################
+## FROM HERE - ONLY FROM 2020-03-11 ARE SHOWN DUE TO NUMBERS OF OBSERVATIONS ##
+###############################################################################
+
+
+########## Comparison of Denmark, Sweden, Romania, Turkey and Philippines##########
 
 
 ### Comparing Denmark, Sweden, Romania, Turkey and Philippines per mill. population
 df_ts %>%
   filter(country %in% c("Denmark","Sweden", "Romania","Turkey","Philippines")) %>%
-  filter(date_observation >= "2020-03-01") %>% # Starting here due to observation
+  filter(date_observation >= "2020-03-11") %>% # Starting here due to observation
   ggplot() +
   geom_line(aes(date_observation, total_confirmed_per_mil_pop, color = country))
 
+### Total deaths per mil pop
+df_ts %>%
+  filter(country %in% c("Denmark","Sweden", "Romania","Turkey","Philippines")) %>%
+  filter(date_observation >= "2020-03-11") %>% # Starting here due to observation
+  ggplot() +
+  geom_line(aes(date_observation, total_deaths_per_mil_pop, color = country))
 
 
 # Model data: Time Series
@@ -175,7 +186,7 @@ df_ts %>%
 #Selecting few countries and nesting per country and time series type
 df_ts_selected<- df_ts %>%
   filter(country %in% c("Denmark","Sweden", "Romania","Turkey","Philippines")) %>%
-  filter(date_observation >= "2020-03-01") %>%
+  filter(date_observation >= "2020-03-11") %>%
   gather(ts, count, total_confirmed:total_deaths_per_mil_pop) %>%
   group_by(country,ts) %>%
   nest()
@@ -187,14 +198,14 @@ df_ts_models<- df_ts_selected %>%
          mdls = map(data, mdl),
          glance = map(mdls,glance),
          tidy = map(mdls,tidy),
-         conf = map(mdls,confint_tidy))
+         conf = map(mdls,confint_tidy),
+         aug = map(mdls,augment))
 
 
-#########################################################################
 ########## Estimate pr. day of confirmed and death per mil pop ##########
 
 
-##### Showing model estimate per confirmed per mil pop
+##### Showing model estimate (coefficient) confirmed per mil pop
 df_ts_models %>%
   unnest(tidy,conf) %>%
   filter(ts == "total_confirmed_per_mil_pop",term == "date_observation") %>%
@@ -204,7 +215,7 @@ df_ts_models %>%
   geom_errorbarh(aes(xmin= conf.low, xmax = conf.high))
 
 
-##### Showing model estimate per confirmed per mil pop
+##### Showing model estimate (coefficient) deaths per mil pop
 df_ts_models %>%
   unnest(tidy,conf) %>%
   filter(ts == "total_deaths_per_mil_pop",term == "date_observation") %>%
@@ -214,13 +225,148 @@ df_ts_models %>%
   geom_errorbarh(aes(xmin= conf.low, xmax = conf.high))
 
 
+##### Evaluation of the models based on the residuals per country
+df_ts_models %>%
+  unnest(aug) %>%
+  select(count,.resid) %>%
+  filter(ts %in% c("total_confirmed_per_mil_pop", "total_deaths_per_mil_pop")) %>%
+  ggplot() +
+  geom_boxplot(aes(country,.resid, fill = country)) +
+  facet_grid(.~ts) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# Model data: Patient data
+# ------------------------------------------------------------------------------
+
+# subsetting data frame for the pca
+df_patient_sub<- df_patient %>%
+  select(gender,age,contact_with_Wuhan:is_recovered,chills:thirst) %>%
+  na.omit() %>%
+  mutate(gender = case_when(gender == 'female' ~ 1,
+                            gender == 'male' ~ 0))
+
+
+# Making PCA of the subset and removing columns that only contains zeros - showing variance
+df_patient_sub %>%
+  select_if(~length(unique(.)) > 1) %>%
+  prcomp(center = TRUE, scale. = TRUE) %>%
+  tidy("pcs") %>%
+  ggplot(aes(x = PC, y = percent)) +
+  geom_col()
+
+
+# Showing decicion tree ????????
+df_patient %>%
+  select(country,gender,age,contact_with_Wuhan:is_recovered,
+         chills:thirst) %>%
+  select_if(~length(unique(.)) > 1) %>% # removing zeros
+  mutate(result = case_when(is_dead == 1 ~ 'dead',
+                            is_dead == 0 ~ 'still_sick',
+                            is_recovered == 1 ~ 'recovered',
+                            is_recovered == 0 ~ 'still_sick')) %>%
+  mutate_if(is.character,as.factor) %>%
+  mutate_if(is.numeric,as.factor) %>%
+  mutate(age = as.integer(age)) %>%
+  select(-is_dead,-is_recovered) %>%
+  rpart(result ~ .,.,method = 'class', model = TRUE,
+        minsplit = 2, minbucket = 1, cp = 0.01) %>%
+  rpart.plot(extra = 101)
+
+
+# Performing logistic regression
+
+df_patient_glm<- df_patient %>%
+  select(country:age,contact_with_Wuhan:is_recovered,
+         chills:thirst) %>%
+  na.omit() %>%
+  select_if(~length(unique(.)) > 1) %>% # removing zeros
+  mutate_if(is.character,as.factor) %>%
+  mutate_if(is.numeric,as.factor) %>%
+  mutate(age = as.integer(age))
+
+
+
+# making model prediction for being dead
+df_patient_glm %>%
+  glm(is_dead ~ ., ., family = binomial()) %>%
+  summary()
+
+
+# selecting significant variables
+df_patient_glm_is_dead<- df_patient_glm %>%
+  select(is_dead, gender, age, contact_with_Wuhan, fever)
+
+# showing the summary (estimate + std.error)
+df_patient_glm_is_dead %>%
+  glm(is_dead ~ ., ., family = binomial()) %>%
+  tidy() %>%
+  mutate(low = estimate - std.error,
+         high = estimate + std.error) %>%
+  ggplot(aes(estimate, term, xmin = low, xmax = high, height = 0)) +
+  geom_point() +
+  geom_vline(xintercept = 0) +
+  geom_errorbarh()
+
+
+# Model!!!
+
+# is_dead = intercept + gender + age + contact_with_wuhan + fever
+
+# is_dead = -9.86 + 0.956*gender + 0.0994*age + 1.64*contact_with_wuhan - 1.64*fever
+
+
+fit<- df_patient_glm_is_dead %>%
+  glm(is_dead ~ ., ., family = binomial())
+
+stanley<- list(gender = as_factor("male"), age = 33,
+              contact_with_Wuhan = as_factor(0), fever = as_factor(0))
+
+predict(fit, stanley, type = "response")
+
+
+
+##### Using augment
+df_patient_glm_is_dead %>%
+  glm(is_dead ~ ., ., family = binomial()) %>%
+  augment()
+
+
+### Plotting hat vs. resid
+df_patient_glm_is_dead %>%
+  glm(is_dead ~ ., ., family = binomial()) %>%
+  augment() %>%
+  ggplot(aes(.hat, .std.resid)) +
+  geom_vline(size = 2, colour = "white", xintercept = 0) +
+  geom_hline(size = 2, colour = "white", yintercept = 0) +
+  geom_point() + geom_smooth(se = FALSE)
+
+
+### Plotting .hat vs. cook's distance
+df_patient_glm_is_dead %>%
+  glm(is_dead ~ ., ., family = binomial()) %>%
+  augment() %>%
+  ggplot(aes(.hat, .cooksd)) +
+  geom_vline(xintercept = 0, colour = NA) +
+  geom_abline(slope = seq(0, 3, by = 0.5), colour = "white") +
+  geom_smooth(se = FALSE) +
+  geom_point()
+
+
+
+df_patient_glm_is_dead %>%
+  glm(is_dead ~ ., ., family = binomial()) %>%
+  glance()
+
 
 
 # Write data
 # ------------------------------------------------------------------------------
+
 #write_tsv(...)
-ggsave(path = "./results",
-       filename = "04_plot.png",
-       plot = bl62_pca_aug_plt,
-       width = 10,
-       height = 6)
+# ggsave(path = "./results",
+#        filename = "04_plot.png",
+#        plot = bl62_pca_aug_plt,
+#        width = 10,
+#        height = 6)
+
