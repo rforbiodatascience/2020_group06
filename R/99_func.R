dd4fd71b1ac05a66a7165473b1769d9d3c2a2bf6
seby-sbirna <seby.sbirna@gmail.com>
2020-05-13 20:02:06 +0200

    update function list

--- R/99_func.R
+++ R/99_func.R
@@ -4,12 +4,3 @@
 mdl <- function(df) {
   lm(count ~ date_observation, data = df)
 }
-
-# SIR model computation
-SIR <- function(date,data,pars) {
-  dS = -beta*I*S
-  dI = beta*I*S - gamma*I
-  dR = gamma*I
-  dD = mu*I
-  return(c(dS,dI,dR,dD))
-}
