93faba9fb6879a9c37ba691efaf97444015eaeb6
Sule <sule@live.dk>
2020-05-13 19:31:24 +0200

    updated do script

--- R/00_do.R
+++ R/00_do.R
@@ -1,5 +1,7 @@
 # Run all scripts --------------------------------------------------------------
 source(file = "R/01_load_and_clean.R")
 source(file = "R/02_augment.R")
-source(file = "R/03_analysis_i.R")
-source(file = "R/04_analysis_ii.R")
+source(file = "R/03_analysis_descriptive.R")
+source(file = "R/04_analysis_SIR.R")
+source(file = "R/05_analysis_symptoms.R")
+
